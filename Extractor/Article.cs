0b46277a42d7bfb3bba6b5cb2509dde1ffca9e9b
Jesús Muela <jciansp@hotmail.com>
2014-11-05 11:46:21 +0100

    Initial commit

---
+++ Extractor/Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extractor
+{
+    public class Article
+    {
+        public Article(Uri link, string thumb, string title, decimal price)
+        {
+            Link = link;
+            Thumb = thumb;
+            Title = title;
+            Price = price;
+        }
+
+        public Article()
+        {
+        }
+
+        public List<string> Categories { get; set; }
+        public Uri Link { get; set; }
+        public Uri Picture { get; set; }
+        public decimal Price { get; set; }
+        public string Thumb { get; set; }
+        public string Title { get; set; }
+        public int Units { get; set; }
+    }
+}
