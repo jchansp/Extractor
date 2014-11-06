32a3091c67890c64afadfda01bba864636bb1c85
Jesús Muela <jciansp@hotmail.com>
2014-11-06 17:49:06 +0100

    Partial commit

---
+++ Extractor.Database/Articles.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Articles]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Link] NVARCHAR(50) NULL, 
+    [Picture] IMAGE NULL, 
+    [Price] MONEY NULL, 
+    [Title] NVARCHAR(50) NULL, 
+    [Units] TINYINT NULL
+)
