f0f3809be7e016ab31b39b80a293f65b0ed56712
Jesús Muela <jciansp@hotmail.com>
2014-11-12 00:42:58 +0100

    Entity Framework Model Connection

--- Extractor/Program.cs
+++ Extractor/Program.cs
@@ -20,16 +20,16 @@ namespace Extractor
         private static void Main()
         {
             var url = new Uri(Scheme + "://" + Host);
-            using (var writer = new StreamWriter("Links.txt", false))
-            {
-                writer.WriteLine();
-            }
-            using (var writer = new StreamWriter("Articles.txt", false))
-            {
-                writer.WriteLine();
-            }
+            //using (var writer = new StreamWriter("Links.txt", false))
+            //{
+            //    writer.WriteLine();
+            //}
+            //using (var writer = new StreamWriter("Articles.txt", false))
+            //{
+            //    writer.WriteLine();
+            //}
             Links = new List<Uri>();
-            Articles = new List<Article>();
+            //Articles = new List<Article>();
             FindArticles(url);
         }
 
@@ -37,41 +37,46 @@ namespace Extractor
         {
             if (url == null || Links.Exists(link => link == url) || url.Host != Host)
                 return;
-            using (var writer = new StreamWriter("Links.txt", true))
-            {
-                writer.WriteLine(url);
-            }
-            Debug.WriteLine(url);
+            //using (var writer = new StreamWriter("Links.txt", true))
+            //{
+            //    writer.WriteLine(url);
+            //}
+            //Debug.WriteLine(url);
             Links.Add(url);
             try
             {
                 Dom = CQ.CreateFromUrl(url.ToString());
                 if (Dom.Find(".opz_product").Length == 1)
                 {
-                    using (var writer = new StreamWriter("Articles.txt", true))
-                    {
-                        writer.WriteLine(url);
-                        writer.WriteLine(ParseUri(Dom.Select(".opz_product img").Get(0).GetAttribute("src")));
-                        writer.WriteLine(Dom.Select(".opz_product .opz_title").Get(0).InnerHTML);
-                        writer.WriteLine(
-                            Convert.ToDecimal(
-                                Dom.Select(".opz_product .lzr_preciofichasart_iva").Get(0).InnerHTML.Split(' ')[0],
-                                new CultureInfo("es-ES")));
-                        writer.WriteLine(Convert.ToInt32(Dom.Select(".opz_product .textounidadesf").Get(0).InnerHTML));
-                        Dom.Select("[rel='category tag']").Each(o => writer.WriteLine(o.InnerHTML));
-                    }
-                    Articles.Add(new Article
+                    //using (var writer = new StreamWriter("Articles.txt", true))
+                    //{
+                    //    writer.WriteLine(url);
+                    //    writer.WriteLine(ParseUri(Dom.Select(".opz_product img").Get(0).GetAttribute("src")));
+                    //    writer.WriteLine(Dom.Select(".opz_product .opz_title").Get(0).InnerHTML);
+                    //    writer.WriteLine(
+                    //        Convert.ToDecimal(
+                    //            Dom.Select(".opz_product .lzr_preciofichasart_iva").Get(0).InnerHTML.Split(' ')[0],
+                    //            new CultureInfo("es-ES")));
+                    //    writer.WriteLine(Convert.ToInt32(Dom.Select(".opz_product .textounidadesf").Get(0).InnerHTML));
+                    //    Dom.Select("[rel='category tag']").Each(o => writer.WriteLine(o.InnerHTML));
+                    //}
+                    Article article = /*new
+                    Articles.Add(*/new Article
                         {
-                            Link = url,
-                            Picture = ParseUri(Dom.Select(".opz_product img").Get(0).GetAttribute("src")),
+                            Link = url.ToString(),
+                            //Picture = ParseUri(Dom.Select(".opz_product img").Get(0).GetAttribute("src")).ToString(),
                             Title = Dom.Select(".opz_product .opz_title").Get(0).InnerHTML,
                             Price =
                                 Convert.ToDecimal(
                                     Dom.Select(".opz_product .lzr_preciofichasart_iva").Get(0).InnerHTML.Split(' ')[0],
                                     new CultureInfo("es-ES")),
-                            Units = Convert.ToInt32(Dom.Select(".opz_product .textounidadesf").Get(0).InnerHTML),
-                            Categories = new List<string>(Dom.Select("[rel='category tag']").Map(o => o.InnerHTML))
-                        });
+                            //Units = Convert.ToInt32(Dom.Select(".opz_product .textounidadesf").Get(0).InnerHTML),
+                            Units = Convert.ToByte(Dom.Select(".opz_product .textounidadesf").Get(0).InnerHTML),
+                            //Categories = new List<string>(Dom.Select("[rel='category tag']").Map(o => o.InnerHTML))
+                        }/*);*/;
+                    DatabaseEntities databaseEntities = new DatabaseEntities();
+                    databaseEntities.Articles.Add(article);
+                    databaseEntities.SaveChanges();
                 }
                 Dom.Select("a")
                    .Each(link => FindArticles(ParseUri(link.HasAttribute("href") ? link.GetAttribute("href") : "")));
